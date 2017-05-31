aae515d59a820c12ef2a6cf968ea63ec0f1bcfde
Francisco <galvan.cfj@gmail.com>
2017-05-31 11:22:26 -0700

    added categories in ui

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
 belongs_to :user
+has_many:article_categories
+has_many:categories, through: :article_categories
 validates :title, presence: true, length: {minimum:2, maximum:300}
 validates :description, presence: true, length: {minimum:10,maximum:5000}
 validates :user_id,presence: true
